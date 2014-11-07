8f0cc94340de863a4c4b2660a080b45fd062b72a
Krzysztof Profic <kprofic@gmail.com>
2014-11-07 14:27:22 +0100

    More robust BDD testing.

--- Example/Tests/TableViewIntentionsTests.m
+++ Example/Tests/TableViewIntentionsTests.m
@@ -15,7 +15,7 @@
 #import <TFIntentions/TFUITableViewDataSourceComposite.h>
 
 
-SpecBegin(ddd);
+SpecBegin(TFUITableViewDataSourceComposite);
 
 describe(@"Composite data source", ^{
     UITableView * tableView = mock([UITableView class]);
@@ -25,10 +25,21 @@ describe(@"Composite data source", ^{
         id<UITableViewDataSource> ds1 = mockProtocol(@protocol(UITableViewDataSource)); // 1 section, 2 rows
         [given([ds1 numberOfSectionsInTableView:tableView]) willReturn:@1];
         [given([ds1 tableView:tableView numberOfRowsInSection:0]) willReturn:@2];
+        UITableViewCell * ds1c00 = mock([UITableViewCell class]);
+        UITableViewCell * ds1c01 = mock([UITableViewCell class]);
+        [given([ds1 tableView:tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]]) willReturn:ds1c00];
+        [given([ds1 tableView:tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:0]]) willReturn:ds1c01];
+        
         id<UITableViewDataSource> ds2 = mockProtocol(@protocol(UITableViewDataSource)); // 2 sections, 0 => 1, 1 => 2
         [given([ds2 numberOfSectionsInTableView:tableView]) willReturn:@2];
         [given([ds2 tableView:tableView numberOfRowsInSection:0]) willReturn:@1];
         [given([ds2 tableView:tableView numberOfRowsInSection:1]) willReturn:@2];
+        UITableViewCell * ds2c00 = mock([UITableViewCell class]);
+        UITableViewCell * ds2c10 = mock([UITableViewCell class]);
+        UITableViewCell * ds2c11 = mock([UITableViewCell class]);
+        [given([ds2 tableView:tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]]) willReturn:ds2c00];
+        [given([ds2 tableView:tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:1]]) willReturn:ds2c10];
+        [given([ds2 tableView:tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:1]]) willReturn:ds2c11];
         
         TFUITableViewDataSourceComposite * compositeDataSource = [[TFUITableViewDataSourceComposite alloc] init];
         compositeDataSource.dataSources = @[ds1, ds2];
@@ -43,14 +54,43 @@ describe(@"Composite data source", ^{
                 expect([compositeDataSource tableView:tableView numberOfRowsInSection:1]).to.equal(2);
             });
             
-            it(@"should return cells from proper underlying dataSources", ^{
-                // TODO
+            it(@"should return the very first cells from first dataSource", ^{
+                expect([compositeDataSource tableView:tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]]).to.equal(ds1c00);
+                expect([compositeDataSource tableView:tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:0]]).to.equal(ds1c01);
+            });
+            
+            it(@"should then return a merged cell from second data source", ^{
+                expect([compositeDataSource tableView:tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:2 inSection:0]]).to.equal(ds2c00);
+            });
+            
+            it(@"should then return cells from second datasource when asked for section nr 2", ^{
+                expect([compositeDataSource tableView:tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:1]]).to.equal(ds2c10);
+                expect([compositeDataSource tableView:tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:1]]).to.equal(ds2c11);
             });
         });
+
         
-        
+        // before mode
         context(@"in join mode ", ^{
-            // before mode
+            it(@"should have number of sections to be a sum of available sections from all underlying data sources", ^{
+                XCTFail();
+            });
+
+            it(@"has a number of rows in sections that corresponds to the underlying values in child data sources", ^{
+                XCTFail();
+            });
+            
+            it(@"should return for the first section all the cells from first section of the first underlying dataSource", ^{
+                XCTFail();
+            });
+            
+            it(@"should then return cells from first section of the second underlyind data source when asked for cells from section nr 2", ^{
+                XCTFail();
+            });
+            
+            it(@"should finally return cells from second section of the second underlying datasource when asked for cells from section nr 3", ^{
+                XCTFail();
+            });
         });
         
     });
